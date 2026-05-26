60833aa0aa80b21c461501b347ce391a152f04bf
KimlayChea <kimlaychea0708@gmail.com>
2026-05-26 16:20:25 +0700

    change url

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,7 +33,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`http://127.0.0.1:3000/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
         },
       },
